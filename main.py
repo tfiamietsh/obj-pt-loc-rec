8de91c32fbe117c3dcaab4814d506aa7d472c303
tfiamietsh <ya.touka2017@yandex.ru>
2026-06-08 09:52:25 +0300

    chore: change default config

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app import App
 
 
 if __name__ == "__main__":
-    config_path = "configs/vino_2st.json"
+    config_path = "configs/onnx_2st.json"
 
     app = App(config_path)
     app.main_loop()
