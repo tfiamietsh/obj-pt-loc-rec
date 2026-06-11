59500ecb2da5d4d2e3d95da7bb6bf3d1c79aca10
tfiamietsh <ya.touka2017@yandex.ru>
2026-06-11 16:51:19 +0300

    feat: integrate new windows to App class

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import cv2
 import json
+import numpy as np
 from engines.dummy_engine import DummyEngine
 from tasks.deeplab_segmenter import DeepLabSegmenter
 from pipelines.pipeline_factory import PipelineFactory
 from tracking.optical_flow import OpticalFlow
 from windows.info_window import InfoWindow
-from utils.fps_counter import FpsCounter
+from windows.viewport import Viewport
 from utils.img_utils import ImgUtils
 
 
@@ -29,35 +30,37 @@ class App:
             colors_bgr=reference_seg.colors_bgr
         )
         self.__smooth_factor = config.get("smooth_factor", 0.6)
-        self.__cap = cv2.VideoCapture(config["camera_id"])
         self.__backend = config.get("backend", "openvino")
-        self.__fps_counter = FpsCounter()
         self.__shutdown_key = "q"
-        self.__app_name = " | ".join([
-            "Viewport",
-            f"{self.__pipeline}",
-            self.__backend,
-            f"press '{self.__shutdown_key}' to exit"
-        ])
-        self.__mask = None
-        self.__frame_idx = 0
+        self.__viewport = Viewport(
+            config=config,
+            name_prefix=" | ".join([
+                "Viewport",
+                f"{self.__pipeline}",
+                self.__backend
+            ]),
+            name_suffix=f"press '{self.__shutdown_key}' to exit"
+        )
+        self.__viewport.set_prerender_fn(self.__process_frame)
         self.__tracker = OpticalFlow()
+        self.__frame_idx = 0
+        self.__mask = None
+
+    def __process_frame(self, frame: np.ndarray):
+        self.__mask, _ = \
+            self.__pipeline.process_frame(frame)
+
+        return ImgUtils.mix(frame, self.__mask, self.__alpha)
 
     def main_loop(self) -> None:
-        while self.__cap and self.__cap.isOpened():
-            ret, frame = self.__cap.read()
-            if not ret:
+        while True:
+            if not self.__viewport.read_frame():
                 break
 
-            self.__mask, _ = \
-                self.__pipeline.process_frame(frame)
+            self.__viewport.render(self.__process_frame)
 
-            masked_frame = ImgUtils.mix(frame, self.__mask, self.__alpha)
-
-            cv2.imshow(self.__app_name, masked_frame)
-
-            self.__fps_counter.update_fps()
-            self.__info_window.set_fps(self.__fps_counter.fps)
+            self.__info_window.set_fps(self.__viewport.get_fps())
+            self.__info_window.render()
 
             if cv2.waitKey(1) & 0xFF == ord(self.__shutdown_key):
                 break
@@ -65,6 +68,5 @@ class App:
             self.__frame_idx += 1
 
     def shutdown(self) -> None:
-        if self.__cap is not None:
-            self.__cap.release()
-            cv2.destroyAllWindows()
+        self.__viewport.close()
+        cv2.destroyAllWindows()
